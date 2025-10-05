9322a424ece05ea43a557c9c7786526419ddfa01
Salmon <salmonvn17@gmail.com>
2025-10-05 23:56:47 +0700

    [ADD] [Python_Basic] Variable

---
+++ Python/01.Basics/01_Variable.py
@@ -0,0 +1,16 @@
+#- ==============================
+#- Topic: Variables
+#- ==============================
+
+#? In Python, variables are created with = or just assignment.
+#? They can store text, numbers, true/false, etc.
+
+name = "Mariam"     # string
+age = 21            # integer
+pi = 3.14           # float
+isStudent = True    # boolean
+
+print('Name:',name)
+print('Age:', age)
+print('Pi', pi)
+print('Student:', isStudent)
